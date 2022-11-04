02573d5f1470ededf248743da9407c3713920897
LuisLagos <luis.lagos.dev@outlook.com>
2022-11-04 15:43:27 -0600

    Add Project Configuration Files

---
+++ src/main/java/com/luis/dev/musikapi/MusikApiApplication.java
@@ -0,0 +1,13 @@
+package com.luis.dev.musikapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusikApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusikApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luis/dev/musikapi/MusikApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.dev.musikapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusikApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
